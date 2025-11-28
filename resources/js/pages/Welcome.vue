18fd7e8be86a8f3428bd0091e9531d6f159600ac
Théo Vandormael <theo.vandormael@proton.me>
2025-11-28 01:16:43 +0100

    feat: remove registration feature and related components

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { dashboard, login, register } from '@/routes';
+import { dashboard, login } from '@/routes';
 import { Head, Link } from '@inertiajs/vue3';
 
 withDefaults(
@@ -37,13 +37,6 @@ withDefaults(
                         class="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
                     >
                         Log in
-                    </Link>
-                    <Link
-                        v-if="canRegister"
-                        :href="register()"
-                        class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
-                    >
-                        Register
                     </Link>
                 </template>
             </nav>
